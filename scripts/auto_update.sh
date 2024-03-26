5eb0750def88fcbfdebca3ce970fc2106464c8eb
Aurainting <aurumting@outlook.com>
2024-03-26 13:49:22 +0800

    server

---
+++ scripts/auto_update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run in the same directory as this script file
+cd .. && git add . && git commit -m "server" && git push
---
+++ scripts/share_in_docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run in the same directory as this script file
+cd ../../ && docker run -it -v xgraph:/opt/xgraph archlinux:latest bash
