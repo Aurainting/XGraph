fba35541af33ad59a7079421daac6a3d2a98af6c
Aurainting <15896359192@163.com>
2024-03-27 20:07:03 +0800

    win

--- include/type_concepts.hpp
+++ include/type_concepts.hpp
@@ -5,7 +5,7 @@
 namespace xgraph {
 
     template<typename T>
-    concept NodeType = requires(T &&x) {
+    concept NodeType = requires(T x) {
         { x.Id() } -> std::convertible_to<std::size_t>;
     };
 
@@ -16,7 +16,7 @@ namespace xgraph {
     };
 
     template<typename T>
-    concept EdgeType = requires(T &&x) {
+    concept EdgeType = requires(T x) {
         { x.Source() } -> NodePtrType;
         { x.Target() } -> NodePtrType;
     };
